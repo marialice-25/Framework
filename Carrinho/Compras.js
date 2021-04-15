bb10f89a4aab8e6af6f52f90a7369b74dd8986c9
Maria Alice <78799728+marialice-25@users.noreply.github.com>
2021-04-15 10:47:21 -0300

    Add files via upload

--- Carrinho/Compras.js
+++ Carrinho/Compras.js
@@ -1,13 +1,15 @@
+
+
 $('#Lista').submit(function(event) {
     event.preventDefault();
  
-    const { id, todo, state } = this.elements;
+    const { id, geral, state } = this.elements;
     
-    if(todo && !id.value) { 
-        const todoSaved = save(todo.value);
-        render(todoSaved.id, todoSaved.text, todoSaved.state);
+    if(geral && !id.value) { 
+        const geralSaved = save(geral.value);
+        render(geralSaved.id, geralSaved.text, geralSaved.state);
     } else {
-        update(id.value, todo.value, state.checked);
+        update(id.value, geral.value, state.checked);
         loadItems();
     }
 
@@ -24,32 +26,32 @@ function getAllTodos() {
 function getById(id) {
     const list = getAllTodos();
     const todo = list.find((item) => item.id == id);
-    if(todo) return todo;
+    if(geral) return geral;
 }
 
 function save(text, state = false) {
-    const todo = {
+    const geral = {
         id: 1,
         text, state
     };
 
     const list = getAllTodos();
-    if(list.length) todo.id = list[list.length - 1].id + 1;
+    if(list.length) geral.id = list[list.length - 1].id + 1;
 
-    list.push(todo);
+    list.push(geral);
     localStorage.setItem('itens', JSON.stringify(list));
-    return todo;
+    return geral;
 }
 
 function deleteItem(id) {
     let list = getAllTodos();
-    list = list.filter(todo => todo.id != id);
+    list = list.filter(geral => geral.id != id);
     localStorage.setItem('itens', JSON.stringify(list));
 }
 
 function update(id, text, state) {
     const list = getAllTodos();
-    const index = list.findIndex((todo) => todo.id == id);
+    const index = list.findIndex((geral) => geral.id == id);
 
     if(index != -1) {
         list[index] = {id, text, state};
@@ -58,20 +60,21 @@ function update(id, text, state) {
     localStorage.setItem('itens', JSON.stringify(list));
 } 
 
-function btnDeleteAction(event) {
+function Deletar(event) {
     deleteItem(event.target.value);
     loadItems();
 }
 
-function btnEditAction(event) {
-    const todo = getById(event.target.value);
+function Incrementar(event) {
+    const geral = getById(event.target.value);
 
-    const formTodo = document.getElementById('Lista');
-    const elements = formTodo.getElementsByTagName('input');
+    const lista = document.getElementById('Lista');
+    const elements = lista.getElementsByTagName('input');
 
-    elements.id.value = todo.id;
-    elements.todo.value = todo.text;
+    elements.id.value = geral.id;
+    elements.geral.value = geral.text;
 }
+
 
 function todoChecked(event) {
     event.target.checked;
@@ -82,8 +85,8 @@ function render(id, text, state = false) {
         <li class="list-group-item" data-id="${id}">
             <input type="checkbox" ${state ? 'checked':  ''} />
             ${text}
-            <button value="${id}" class="botao2" type="submit" name="deleta">🗑</button>
-            <button value="${id}" class="botao4" type="submit" name="edita">✏</button>
+            <button value="${id}" class="botao5" type="submit" name="deleta">DELETAR</button>
+            <button value="${id}" class="botao4" type="submit" name="incrementa">INCREMENTAR</button>
         </li>
     `);
 }
@@ -91,11 +94,11 @@ function render(id, text, state = false) {
 function loadItems() {
     $('.list-group').empty();
 
-    getAllTodos().forEach((todo) => {
-        render(todo.id, todo.text, todo.state);
+    getAllTodos().forEach((geral) => {
+        render(geral.id, geral.text, geral.state);
 
-        $('.botao2').click(btnDeleteAction);
-        $('.botao4').click(btnEditAction);
+        $('.botao5').click(Deletar);
+        $('.botao4').click(Incrementar);
     });
 } 
 
